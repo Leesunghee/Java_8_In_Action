5dd25af5a59b768cc9b8afcbe7c612b921ff020a
이성희 <realwunz@hyundai-steel.com>
2018-11-07 09:10:38 +0900

    add Stream example files Dish, StreamMain

---
+++ src/main/java/com/ready2die/stream/StreamMain.java
@@ -0,0 +1,46 @@
+package com.ready2die.stream;
+
+import java.util.*;
+
+public class StreamMain {
+
+
+    public static void main(String[] args) {
+
+        List<Dish> menu = Arrays.asList(
+                new Dish("pork", false, 800, Dish.Type.MEAT),
+                new Dish("beef", false, 700, Dish.Type.MEAT),
+                new Dish("chicken", false, 400, Dish.Type.MEAT),
+                new Dish("french fries", true, 530, Dish.Type.OTHER),
+                new Dish("rice", false, 350, Dish.Type.OTHER),
+                new Dish("season", false, 120, Dish.Type.OTHER),
+                new Dish("pizza", false, 550, Dish.Type.OTHER),
+                new Dish("prawns", false, 300, Dish.Type.FISH),
+                new Dish("salmon", false, 450, Dish.Type.FISH)
+        );
+
+        List<Dish> lowCaloricDishes = new ArrayList<>();
+        for (Dish d : menu) {
+            if (d.getCalories() < 400) {
+                lowCaloricDishes.add(d);
+            }
+        }
+        Collections.sort(lowCaloricDishes, new Comparator<Dish>(){
+
+            @Override
+            public int compare(Dish d1, Dish d2) {
+                return Integer.compare(d1.getCalories(), d1.getCalories());
+            }
+        });
+
+        List<String> lowCaloricDishesName = new ArrayList<>();
+        for (Dish d : lowCaloricDishes) {
+            lowCaloricDishesName.add(d.getName());
+        }
+    }
+
+
+
+
+
+}
